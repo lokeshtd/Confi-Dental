6162ef278fa91e66b3bdb61501cdc847b76bd8f9
lokeshtd <lokeshthanigai250@gmail.com>
2024-05-24 09:18:22 +0530

    changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <header>
       <nav className='nav'>
-      <button type='button' className='login-home-btn'>Login/Sign up</button>
+      {/* <button type='button' className='login-home-btn'>Login/Sign up</button> */}
       <img src={logo} className='logo' alt='' />
       <h1 className='confi'>Confi <span className='head'>Dental Clinic</span></h1>
         <ul >
